fccb0c4555e86ff51772f0bfca76b871cdecb919
pgodlewski <pawel.g@survicate.com>
2025-10-29 09:36:59 +0100

    Reverted changes for verification test

--- ios/survicate_sdk/Package.swift
+++ ios/survicate_sdk/Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         .library(name: "survicate-sdk", targets: ["survicate_sdk"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Survicate/survicate-ios-sdk.git", exact: "6.4.2")
+        .package(url: "https://github.com/Survicate/survicate-ios-sdk.git", exact: "6.4.3")
     ],
     targets: [
         .target(
